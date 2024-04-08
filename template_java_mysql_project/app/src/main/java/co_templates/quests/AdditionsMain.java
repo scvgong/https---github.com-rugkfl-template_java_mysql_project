a1fff9f1e35d36ac741acd7f22f54c848f5a0426
scvgong <gongmyeongyun@gmail.com>
2024-04-08 05:16:28 +0000

    add

--- template_java_mysql_project/app/src/main/java/co_templates/quests/AdditionsMain.java
+++ template_java_mysql_project/app/src/main/java/co_templates/quests/AdditionsMain.java
@@ -4,6 +4,6 @@ public class AdditionsMain {
     public static void main(String[] args) {
        AdditionsSub addSub = new AdditionsSub();
        int result = addSub.additions();
-       System.out.println(" 두 정수의 합 : " + result);
+       System.out.println("두 정수의 합 : " + result);
     }
 }
